42f54bb5c07158fb24ddcbbcf7b27681c904ab94
mnsalimi <sartakhti.salimi@gmail.com>
2022-06-05 01:25:00 +0430

    finalll

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"main.dart.js": "ba4669c1add2e7932ce7b860298c9be2",
+"main.dart.js": "cfcded117a0163f853f4dde31781fb2d",
 "manifest.json": "ac1117c30de9989c945de2b976d8d664",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -21,8 +21,8 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"index.html": "e42622b2e702e8e0b55ce37836db6606",
-"/": "e42622b2e702e8e0b55ce37836db6606"
+"index.html": "adc5ca4702fef820af2c1343e3364ab5",
+"/": "adc5ca4702fef820af2c1343e3364ab5"
 };
 
 // The application shell files that are downloaded before a service worker can
